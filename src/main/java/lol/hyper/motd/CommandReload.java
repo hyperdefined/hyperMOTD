f193f45a8e0fe090ada1e3ca453392644c8377e1
hyperdefined <13475823+hyperdefined@users.noreply.github.com>
2020-07-06 13:30:49 -0400

    Added files

---
+++ src/main/java/lol/hyper/motd/CommandReload.java
@@ -0,0 +1,18 @@
+package lol.hyper.motd;
+
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.plugin.Command;
+
+public class CommandReload extends Command {
+    public CommandReload(String name) {
+        super("motdreload");
+    }
+
+    @Override
+    public void execute(CommandSender sender, String[] args) {
+        MOTD.getInstance().loadConfig();
+        sender.sendMessage(new TextComponent(ChatColor.GREEN + "Config reloaded!"));
+    }
+}
---
+++ src/main/java/lol/hyper/motd/MOTD.java
@@ -0,0 +1,74 @@
+package lol.hyper.motd;
+
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.ServerPing;
+import net.md_5.bungee.api.event.ProxyPingEvent;
+import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.config.Configuration;
+import net.md_5.bungee.config.ConfigurationProvider;
+import net.md_5.bungee.config.YamlConfiguration;
+import net.md_5.bungee.event.EventHandler;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+public final class MOTD extends Plugin implements Listener {
+
+    private static MOTD instance;
+    List<String> motds = new ArrayList<>();
+    String fixedMOTD;
+    String type;
+
+    public static MOTD getInstance() {
+        return instance;
+    }
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        loadConfig();
+        ProxyServer.getInstance().getPluginManager().registerListener(this, this);
+        getProxy().getPluginManager().registerCommand(this, new CommandReload("motdreload"));
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    @EventHandler
+    public void onPing(ProxyPingEvent e) {
+        ServerPing response = e.getResponse();
+        /* NOT TESTED
+        int players = getProxy().getServerInfo("main").getPlayers().size();
+        ServerPing.Players players2 = response.getPlayers();
+        players2.setOnline(players);
+        response.setPlayers(players2);
+        */
+        if (type.equalsIgnoreCase("random")) {
+            int randomNum = ThreadLocalRandom.current().nextInt(0, motds.size());
+            String motd = ChatColor.translateAlternateColorCodes('&', motds.get(randomNum));
+            response.setDescription(motd);
+        } else if (type.equalsIgnoreCase("fixed")) {
+            String motd = ChatColor.translateAlternateColorCodes('&', fixedMOTD);
+            response.setDescription(motd);
+        }
+    }
+
+    public void loadConfig() {
+        try {
+            File configFile = new File("plugins//MOTD", "config.yml");
+            Configuration configuration = ConfigurationProvider.getProvider(YamlConfiguration.class).load(configFile);
+            type = configuration.getString("type");
+            motds = configuration.getStringList("random-motd");
+            fixedMOTD = configuration.getString("fixed-motd");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
